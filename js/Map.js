5b0bcb729ecd5c7a4fa760866225946c0e5d96ca
zk-qd <2605066922@qq.com>
2020-04-04 15:27:55 +0800

    newly content

--- js/Map.js
+++ js/Map.js
@@ -15,9 +15,10 @@ void function (window) {
             for (let key of Reflect.ownKeys(source)) {
                 if (key !== 'constructor'
                     && key !== 'prototype'
-                    && (key !== 'name'
-                        || key === 'name' && source !== 'function')
+                    && !(key === 'name' && typeof source === 'function')
+                    && !(key === 'length' && typeof source === 'function')
                 ) {
+                    console.log(source, key)
                     let desc = Object.getOwnPropertyDescriptor(source, key);
                     Object.defineProperty(target, key, desc);
                 }
@@ -30,7 +31,11 @@ void function (window) {
             if (config && typeof config === 'function') {
                 callback = config;
                 config = {};
-            } else if (!onfig.Object.create.toString() === ['Object object']) throw new MapError('Wrong second parameter');
+            } else if (!onfig.Object.create.toString() === ['Object object']) MapError.config3callbackExeception();
+            return {
+                callback,
+                config,
+            }
         }
     }
     // 无地图也能使用
@@ -38,30 +43,31 @@ void function (window) {
 
     }
     // 地图调用的方法
+    const geocoder = Symbol('geocoder');
     class Instance {
         constructor() {
             if (new.target === Map) throw TypeError('本类不能实例化');
-            // 地理编码
-            this.geocoder = null;
         }
+        // 地理编码
+        [geocoder] = null;
         // 添加基础工具
         addBasicTools() {
             // 在图面添加工具条控件，工具条控件集成了缩放、平移、定位等功能按钮在内的组合控件
-            this.map.addControl(new this.$KMap.ToolBar());
+            this.$map.addControl(new KMap.AMap.ToolBar());
 
             // 在图面添加比例尺控件，展示地图在当前层级和纬度下的比例尺
-            this.map.addControl(new this.$KMap.Scale());
+            this.$map.addControl(new KMap.AMap.Scale());
 
             // 在图面添加鹰眼控件，在地图右下角显示地图的缩略图
-            this.map.addControl(new this.$KMap.OverView({ isOpen: true }));
+            this.$map.addControl(new KMap.AMap.OverView({ isOpen: true }));
 
             // 在图面添加类别切换控件，实现默认图层与卫星图、实施交通图层之间切换的控制
-            this.map.addControl(new this.$KMap.MapType());
+            this.$map.addControl(new KMap.AMap.MapType());
             // 在图面添加定位控件，用来获取和展示用户主机所在的经纬度位置
-            // map.addControl(new $AMap.Geolocation());
+            this.$map.addControl(new KMap.AMap.Geolocation());
         }
         createGeocoder(config) {
-            this.geocoder = new this.$KMap.Geocoder({
+            this[geocoder] = new KMap.AMap.Geocoder({
                 city: "长沙", //城市设为北京，默认：“全国”
                 radius: 1000, //范围，默认：500
                 lnag: 'zh-CN',
@@ -72,48 +78,73 @@ void function (window) {
         }
         // 地理编码
         getLocation(address, config, callback) {
-            MapScheme.config3callback(config, callback);
-            if (!this.geocoder)
+            const result = MapScheme.config3callback(config, callback);
+            config = result.config;
+            callback = result.callback
+            if (!this[geocoder])
                 this.createGeocoder(config);
             // 传入数组批量查询 传入字符串查单个
-            this.geocoder.getLocation(address, function (status, result) {
+            this[geocoder].getLocation(address, function (status, result) {
                 if (status === 'complete' && result.geocodes.length)
-                    callback || callback(result.geocodes[0].location)
-                else
+                    callback && callback(result.geocodes[0].location)
+                else {
                     console.error('根据地址查询经纬度失败');
+                    callback && callback(null)
+                }
             });
         }
         // 逆向地理编码
         getAddress(lnglat, config, callback) {
-            MapScheme.config3callback(config, callback);
-            if (!this.geocoder)
+            const result = MapScheme.config3callback(config, callback);
+            config = result.config;
+            callback = result.callback
+            if (!this[geocoder])
                 this.createGeocoder(config);
             // 传入[[],[]]批量查或者 传入[]查询单个
-            this.geocoder.getAddress(lnglat, function (status, result) {
+            this[geocoder].getAddress(lnglat, function (status, result) {
                 if (status === 'complete' && result.regeocode)
-                    callback(result.regeocode.formattedAddress);
-                else
+                    callback && callback(result.regeocode.formattedAddress);
+                else {
                     console.error('根据经纬度查询地址失败');
+                    callback && callback('')
+                }
+
             });
         }
     }
 
     // 错误
     class MapError extends Error {
-        constructor(message, location) {
+        constructor(message, location = '') {
             super();
             this.name = 'MapError';
             this.message = message;
-            this.stack = this.name + ':' + this.message + location;
+            // Map.js:104 Uncaught MapError:selector[object HTMLDivElement]element is not undefiend
+            this.stack = this.name + ': \n' + this.message + location;
         }
+        // plugin
+        static pluginExeception() {
+            throw new MapError('Please pass in the correct one "plugin"', '\nloader');
+        }
+        // container
+        static containerExeception(container) {
+            throw new MapError('selector "#' + container +
+                '" element is not undefiend', '\nbuild map fail')
+        }
+        // config and callback
+        static config3callbackExeception() {
+            throw new MapError('Wrong second parameter', 'geocoder or ...')
+        }
+
     }
     class KMap extends Mixin(Instance, Static) {
         constructor() {
             super();
             // 地图
             this.$map = null;
-            this.$KMap = null;
         }
+        // 地图类
+        static AMap
         // 回调
         static loader({
             key = 'a1aba2049ce8ef3ed0dd419dd839b4bb',
@@ -122,34 +153,49 @@ void function (window) {
             container = null,
             center = null,
         } = {}) {
-            new Promise((resolve, reject) => {
-                // 基础插件配置
-                var basePlugin = 'AMap.ToolBar,AMap.Scale,AMap.OverView,AMap.MapType,AMap.Geolocation,';
-                // plugin = [AMap.Geocoder,]
+            return new Promise((resolve, reject) => {
+                // basic plugin
+                var basicPlugins = ['AMap.ToolBar', 'AMap.Scale', 'AMap.OverView', 'AMap.MapType', 'AMap.Geolocation'];
+                // extensions plugin
+                var extensionPlugins = function () {
+                    // all plugin
+                    if (plugin === 'all') plugin = ['AMap.Geocoder']
+                    // not array
+                    else if (!plugin instanceof Array)
+                        MapError.pluginExeception();
+                    return basicPlugins.concat(plugin).join(',').trim();
+                }
+                // dynamic build
                 var script = document.createElement('script'),
                     src = 'http://webapi.amap.com/maps?v=1.4.14&key=' + key +
-                        '&plugin=' + basePlugin + plugin.join(',').trim() + '&callback=mapLoadingComplete';
+                        '&plugin=' + extensionPlugins() + '&callback=mapLoadingComplete';
                 script.type = 'text/javascript';
                 script.src = src;
                 script.async = true;
                 document.body.appendChild(script);
+                // success callback
                 window.mapLoadingComplete = function () {
+                    // static
+                    let kmap = null;
+                    // save AMap class
+                    KMap.AMap = window.AMap;
                     // static to singleton
                     function Static2Singleton() {
                         // build instance 
-                        const kmap = new KMap();
-                        // save inner  
-                        kmap.$KMap = window.AMap;
-                        // verify container
-                        container || (new MapError('container is empty'));
+                        kmap = new KMap();
                         // build map, save inner
-                        kmap.$map = new AMap(container, {
-                            center: center,
+                        kmap.$map = new AMap.Map(container, {
+                            center,
                         })
-                        return kmap;
+                    }
+                    if (container) {
+                        document.getElementById(container) || MapError.containerExeception(container);
+                        // start singleton
+                        Static2Singleton();
                     }
                     resolve(kmap);
                 }
+                // error callback
                 script.addEventListener('error', function (e) {
                     reject(e);
                 }, {
